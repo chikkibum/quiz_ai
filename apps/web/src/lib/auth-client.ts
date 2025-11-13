e665eb61cc174400fd6dffa86fb8282a1f4881b3
chikkibum <bhaskarpandeycontacts@gmail.com>
2025-11-14 01:01:46 +0530

    rbac better auth

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import type { auth } from "@my-better-t-app/auth";
 import { createAuthClient } from "better-auth/react";
-import { inferAdditionalFields } from "better-auth/client/plugins";
+import { inferAdditionalFields, adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-	plugins: [inferAdditionalFields<typeof auth>()],
+	plugins: [inferAdditionalFields<typeof auth>(), adminClient()],
 });
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,6 +2,7 @@ import { nextCookies } from 'better-auth/next-js';
 import { betterAuth, type BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@my-better-t-app/db";
+import { admin } from 'better-auth/plugins';
 
 export const auth = betterAuth<BetterAuthOptions>({
 	database: prismaAdapter(prisma, {
@@ -11,5 +12,5 @@ export const auth = betterAuth<BetterAuthOptions>({
 	emailAndPassword: {
 		enabled: true,
 	},
-  plugins: [nextCookies()]
+  plugins: [nextCookies(),admin()]
 });

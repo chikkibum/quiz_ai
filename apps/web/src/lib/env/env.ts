7276890fcc43d6646d27add87e73d33113194312
chikkibum <bhaskarpandeycontacts@gmail.com>
2025-11-16 17:52:14 +0530

    onfig changes

--- apps/web/src/lib/env/env.ts
+++ apps/web/src/lib/env/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DIRECT_URL: z.string().url().startsWith("postgres://"),
+    DIRECT_URL: z.url().startsWith("postgres://"),
   },
   runtimeEnv: process.env,
 });
